d8c7053dc2c70438bd5d4793f44a312ba7f2a3cd
brian978 <dbrian89@yahoo.com>
2012-09-21 20:03:20 +0300

    Added some new functions

---
+++ SJL/ActionTrigger/actiontrigger.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * A class that triggeres a set action when a set container changes it's state
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ * @name ActionTriger
+ * @version 1.0
+ *
+ */
--- SJL/Common/common.js
+++ SJL/Common/common.js
@@ -13,6 +13,53 @@ function isset(variable){
     } else {
 
         return false;
-        
+
     }
+}
+
+/**
+ * The function does a redirect
+ *
+ * @param {String} url
+ * @return void
+ */
+function redirect(url){
+    location.replace(url);
+}
+
+/**
+ * The function checks if the given value is numeric
+ *
+ * @param {String} n
+ * @return boolean
+ */
+function is_numeric(n)
+{
+    // ==== List of valid characters for a number ==== //
+    var validChars = "0123456789.-";
+
+    // ==== Current character in loop ==== //
+    var cChar;
+
+    // ==== Check variable ==== //
+    var result = true;
+
+    // ==== Checking the string length ==== //
+    if (n.length == 0) result = false;
+
+    // ==== Testing if the string contains valid characters ==== //
+    for (var i = 0; i < n.length && result == true; i++)
+    {
+        // ==== Getting the current character ==== //
+        cChar = n.charAt(i);
+
+        // ==== Checking if the character exists in the valid chars string ==== //
+        if (validChars.indexOf(cChar) == -1)
+        {
+            result = false;
+        }
+    }
+
+    // ==== result ==== //
+    return result;
 }
