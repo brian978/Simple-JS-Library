e13890fbffd972024d7c454732b0aa5dab11799a
brian978 <dbrian89@yahoo.com>
2012-09-26 21:06:56 +0300

    Added LastAction class (wrapper for the Action class - the difference is it is a static class)

---
+++ lastaction.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * Class that stores the last action and allows you to call it again in case the process gets interrupted
+ *
+ * @author Brian
+ * @link https://github.com/brian978
+ * @copyright 2012
+ * @license Creative Commons Attribution-ShareAlike 3.0
+ *
+ * @name LastAction
+ * @version 1.0.1
+ *
+ */
+
+/**
+ * Class constructor
+ *
+ * @param {Void}
+ * @return void
+ */
+function LastAction(){}
+
+/**
+ * Used to register the last action
+ *
+ * @param {Object} objInstance
+ * @param {String} methodName
+ * @param {Array} params
+ * @return void
+ */
+LastAction.register = function(objInstance, methodName, params){
+
+    // Action
+    if(typeof LastAction.action == 'undefined'){
+        LastAction.action = new Action();
+    }
+
+    // Registering the action
+    LastAction.action.register(objInstance, methodName, params);
+}
+
+/**
+ * Executes the last action
+ *
+ * @param {Void}
+ * @return boolean
+ */
+LastAction.execute = function(){
+
+    // Executing and returning the result
+    return LastAction.action.execute();
+}
